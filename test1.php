790c267ae8ba8baf11ab4b45b18f812bb533af90
 <seiji.mnsm@gmail.com>
2024-09-12 16:41:52 +0900

    変更してみた

---
+++ test1.php
@@ -0,0 +1,17 @@
+<?php
+
+  $data = 80;
+  switch ($data) {
+    case 60:
+      echo "C";
+    break;
+    case 70:
+      echo "B";
+    break;
+    case 80:
+      echo "A";
+    break;
+    default:
+      echo "D";
+  }
+?>
